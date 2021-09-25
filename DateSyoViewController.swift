84d64df6d94334587f7ac401bd06306a0d6db5fa
Kyohei <gyoroemon0929@gmail.com>
2021-09-26 04:10:22 +0900

    編集(上書き)、イベント削除機能、IDによるフィルター 追加機能はまだ

---
+++ DateSyoViewController.swift
@@ -0,0 +1,145 @@
+//
+//  DateSyoViewController.swift
+//  Hakadorikun
+//
+//  Created by 奥若菜 on 2021/04/10.
+//
+
+import UIKit
+import RealmSwift
+
+class DateSyoViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    var SelectDate:Date? = nil
+    
+
+    @IBOutlet weak var eventTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        eventTable.delegate = self
+        eventTable.dataSource = self
+        
+    }
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let ModifiedDate = Calendar.current.date(byAdding: .day, value: 1, to: SelectDate!)!
+        
+        let realm = try! Realm()
+        let data = realm.objects(EventObj.self).filter("%@ < start_time && start_time < %@ || %@ < end_time && end_time < %@ || start_time < %@ && end_time > %@",SelectDate as Any,ModifiedDate,SelectDate as Any,ModifiedDate,SelectDate as Any,ModifiedDate)
+        
+        if(data.count == 0){
+            return 1
+        } else{
+            return data.count
+        }
+        
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let title:[String] = ["イベント","課題"]
+        return title[section]
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let ModifiedDate = Calendar.current.date(byAdding: .day, value: 1, to: SelectDate!)!
+        
+        let realm = try! Realm()
+        
+        if indexPath.section == 0{
+            let data = realm.objects(EventObj.self).filter("%@ < start_time && start_time < %@ || %@ < end_time && end_time < %@ || start_time < %@ && end_time > %@",SelectDate as Any,ModifiedDate,SelectDate as Any,ModifiedDate,SelectDate as Any,ModifiedDate)
+            
+            if(data.count != 0){
+                
+                let object = data[indexPath.row]
+                
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Eventcell",
+                                                             for: indexPath)
+                
+                let labelTitle = cell.viewWithTag(1) as! UILabel
+                labelTitle.text = object.title
+                
+                let labelPlace = cell.viewWithTag(2) as! UILabel
+                labelPlace.text = object.place
+                
+                let timeformatter = DateFormatter()
+
+                timeformatter.locale = Locale(identifier: "ja_JP")
+                timeformatter.dateFormat = "H:mm"
+                
+                let labelStime = cell.viewWithTag(3) as! UILabel
+                labelStime.text = timeformatter.string(from: object.start_time! )
+                
+                let labelEtime = cell.viewWithTag(4) as! UILabel
+                labelEtime.text = timeformatter.string(from: object.end_time! )
+                
+                return cell
+                
+            } else {
+                
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Eventcell",
+                                                             for: indexPath)
+                
+                let labelTitle = cell.viewWithTag(1) as! UILabel
+                labelTitle.text = "新規イベント"
+                
+                return cell
+            }
+        } else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Eventcell",
+                                                         for: indexPath)
+            return cell
+        }
+        
+        
+    }
+    
+    func tableView(_ table: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    //cellをタップした時の処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let ModifiedDate = Calendar.current.date(byAdding: .day, value: 1, to: SelectDate!)!
+        
+        let realm = try! Realm()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let data = realm.objects(EventObj.self).filter("%@ < start_time && start_time < %@ || %@ < end_time && end_time < %@ || start_time < %@ && end_time > %@",SelectDate as Any,ModifiedDate,SelectDate as Any,ModifiedDate,SelectDate as Any,ModifiedDate)
+        
+        let EventViewController = self.storyboard?.instantiateViewController(withIdentifier: "EventViewController") as! EventViewController
+        
+        EventViewController.ID = data[indexPath.row].ID
+        
+        self.navigationController?.pushViewController(EventViewController, animated: true)
+        
+    }
+    
+    
+    
+    @IBOutlet weak var Update: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      //テーブルを再描画
+      Update.reloadData()
+    }
+
+}
---
+++ EventViewController.swift
@@ -0,0 +1,119 @@
+//
+//  EventViewController.swift
+//  Hakadorikun
+//
+//  Created by 奥若菜 on 2021/05/29.
+//
+
+import UIKit
+//import TTTAttributedLabel
+import RealmSwift
+
+
+class EventViewController: UIViewController, UITextViewDelegate {
+    
+    var ID:String? = nil
+    
+    let realm = try! Realm()
+    
+    var AllDay:Int? = nil
+    
+
+    @IBOutlet weak var Memoview: UITextView!
+    @IBOutlet weak var URLView: UILabel!
+    @IBOutlet weak var EventTitle: UILabel!
+    
+    @IBOutlet weak var LocalTitle: UILabel!
+    @IBOutlet weak var LocalAdress: UILabel!
+    
+    @IBOutlet weak var TimeLabel1: UILabel!
+    @IBOutlet weak var TimeLabel2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        Memoview.delegate =  self as UITextViewDelegate
+        
+        let timeformatter = DateFormatter()
+
+        timeformatter.locale = Locale(identifier: "ja_JP")
+        timeformatter.dateFormat = "H:mm"
+
+        let dateformatter = DateFormatter()
+
+        dateformatter.locale = Locale(identifier: "ja_JP")
+        dateformatter.dateFormat = "yyyy年M月d日 EEE曜日"
+
+        let data = realm.objects(EventObj.self).filter("%@ == ID",ID as Any)
+
+        if (data.count != 0){
+
+            EventTitle.text = data.last?.title
+            LocalTitle.text = data.last?.place
+            LocalAdress.text = data.last?.address
+            URLView.text = data.last?.url
+            Memoview.text = data.last?.memo
+
+            let Stime = data.last?.start_time
+            let Etime = data.last?.end_time
+            let AllDay = data.last?.all_day
+
+            if Stime == nil || Etime == nil{
+
+            } else {
+
+
+                if dateformatter.string(from: Stime!) == dateformatter.string(from: Etime!){
+
+                    if AllDay == 1{
+                        //終日
+                        TimeLabel1.text = "\(dateformatter.string(from: Stime!))"
+                        TimeLabel2.text = "終日"
+
+                    } else {
+                        //日付同じで、終日ではない
+                        TimeLabel1.text = dateformatter.string(from: Stime!)
+                        TimeLabel2.text = "\(timeformatter.string(from: Stime!))〜\(timeformatter.string(from: Etime!))"
+                    }
+
+                } else {
+                    if AllDay == 1{
+                        //終日、日付が異なる
+                        TimeLabel1.text = "\(dateformatter.string(from: Stime!))から"
+                        TimeLabel2.text = "\(dateformatter.string(from: Etime!))まで"
+
+                    } else {
+                        //日付が異なる、終日ではない
+                        TimeLabel1.text = "\(dateformatter.string(from: Stime!)) \(timeformatter.string(from: Stime!))"
+                        TimeLabel2.text = "\(dateformatter.string(from: Etime!)) \(timeformatter.string(from: Etime!))"
+                    }
+                }
+            }
+        } else {
+
+        }
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+        
+        viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func EditButton(_ sender: Any) {
+        
+        let EventEditTableViewController = self.storyboard?.instantiateViewController(withIdentifier: "EventEditTableViewController") as! EventEditTableViewController
+        
+        EventEditTableViewController.ID = ID
+        
+        self.navigationController?.pushViewController(EventEditTableViewController, animated: true)
+        
+    }
+    
+
+}
